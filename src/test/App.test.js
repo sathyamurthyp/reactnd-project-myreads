5aa5ab1018e8fb5662e2ae4d4b359bf516472882
sathyamurthyp <sathyamurthyp@gmail.com>
2017-08-06 22:40:53 +0530

    Update Tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
-import BookList from '../components/BookList'
+import BookShelf from '../components/BookShelf'
 import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
 
 describe('A suite', function() {
   it('should render without throwing an error', function() {
-  	const wrapper = shallow(<BookList bookList={books} onChangeShelf={onChangeShelf} />);
-    expect(wrapper.find('li').children()).to.have.length(bookList.length);
+  	const wrapper = shallow(<App />);
+    expect(wrapper.containsAnyMatchingElements(<BookShelf/>).toBe(true);
   });
 
